e160a320eaa11f3d118ae9814f56e02fdb7b4f90
Alex <2703parker@gmail.com>
2024-12-06 22:29:22 -0600

    cleanup task1.sh

--- HW07/task1.sh
+++ HW07/task1.sh
@@ -1,14 +1,34 @@
 #!/usr/bin/env bash
+
 #SBATCH --job-name=task1
+
 #SBATCH -p instruction
+
 #SBATCH --ntasks=1 --cpus-per-task=2
-#SBATCH --time=0-00:10:00
+
+#SBATCH --time=0-00:30:00
+
 #SBATCH --output="./task1.out"
+
 #SBATCH --error="./task1.err"
+
 #SBATCH --gres=gpu:1
 
+set -e
+
+# Load necessary modules
 module load nvidia/cuda/11.8.0
+
 module load gcc/9.4.0
+
+# Compile the program
 nvcc task1.cu matmul.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o task1
-./task1 8192 32
+
+# Run the program for n = 2^5 to 2^14
+for ((i=5; i<=14; i++)); do
+    n=$((2**i))
+    ./task1 $n 16 >> task1_b.out
+done
+
+# Clean up
 rm task1
