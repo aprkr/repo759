258c2eb1588204c9fab979f1933726839d73a7d2
Alex <2703parker@gmail.com>
2024-12-06 22:52:19 -0600

    fix task2 script

--- HW07/task2.sh
+++ HW07/task2.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=task2_b
+
+#SBATCH --job-name=task2
+
 #SBATCH -p instruction
+
 #SBATCH --ntasks=1 --cpus-per-task=2
+
 #SBATCH --time=0-00:30:00
-#SBATCH --output="./task2_b.out"
-#SBATCH --error="./task2_b.err"
+
+#SBATCH --output="./task2.out"
+
+#SBATCH --error="./task2.err"
+
 #SBATCH --gres=gpu:1
+
+set -e
 
 # Load necessary modules
 module load nvidia/cuda/11.8.0
@@ -17,17 +26,13 @@ nvcc task2.cu reduce.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -
 # Run the program for n = 2^10 to 2^30 with threads_per_block=256
 for ((i=10; i<=29; i++)); do
     n=$((2**i))
-    echo "Running task2 for n = $n with threads_per_block=256" >> task2_b.out
-    ./task2 $n 256 >> task2_b.out
-    echo "Finished task2 for n = $n with threads_per_block=256" >> task2_b.out
+    ./task2 $n 256
 done
 
 # Run the program for n = 2^10 to 2^30 with threads_per_block=1024
 for ((i=10; i<=29; i++)); do
     n=$((2**i))
-    echo "Running task2 for n = $n with threads_per_block=1024" >> task2_b.out
-    ./task2 $n 1024 >> task2_b.out
-    echo "Finished task2 for n = $n with threads_per_block=1024" >> task2_b.out
+    ./task2 $n 1024
 done
 
 # Clean up
